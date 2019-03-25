69a2d63df2d3cf261491893362bbca35ffc00d47
dboonstra177 <derekboonstra@yahoo.com>
2019-03-24 22:12:07 -0500

    Add files via upload

---
+++ action_page.php
@@ -0,0 +1,10 @@
+
+<html>
+    <body>
+    Welcome <?php echo $_POST["firstname"]; ?><br>
+    <?php echo $_POST["lastname"]; ?>
+    Your from: <?php echo $_POST["city"]; ?>
+    Your Order Details: <?php echo $_POST["subject"]; ?>
+    
+</body>
+</html>
